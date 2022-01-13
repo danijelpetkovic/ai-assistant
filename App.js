ef07f95b293706ceb99b08d626660c57e9570d12
Danijel Petkovic <danijel.petkovic@neta.io>
2022-01-13 12:18:40 +0100

    Try fetch

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 
-import React from "react";
-import { View, StyleSheet, TextInput, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, TextInput, Text, ActivityIndicator, FlatList } from "react-native";
 
 export default function App() {
   const [text, onChangeText] = React.useState("");
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const getContext = async () => {
+    try {
+     const response = await fetch('https://reactnative.dev/movies.json');
+     const json = await response.json();
+     setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getContext();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -14,6 +32,15 @@ export default function App() {
         value={text}
         placeholder="Enter a question"
       />
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
     </View>
   );
 };
